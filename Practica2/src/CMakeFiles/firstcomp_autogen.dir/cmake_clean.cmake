588893f701c01b77fe059f164caa0d0f3206d351
Manuel <Manuel Davila>
2018-11-08 19:06:29 +0100

    Práctica 2

---
+++ Practica2/src/CMakeFiles/firstcomp_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "firstcomp_autogen"
+  "CMakeFiles/firstcomp_autogen.dir/AutogenOldSettings.cmake"
+  "CMakeFiles/firstcomp_autogen"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/firstcomp_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
