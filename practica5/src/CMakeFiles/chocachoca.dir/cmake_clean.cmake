efe04ebb16a4f54c5b9c694dca91734c0580ebcd
Manuel <Manuel Davila>
2018-11-28 18:05:35 +0100

    Práctica 5

---
+++ practica5/src/CMakeFiles/chocachoca.dir/cmake_clean.cmake
@@ -0,0 +1,38 @@
+file(REMOVE_RECURSE
+  "chocachoca_autogen"
+  "CMakeFiles/chocachoca_autogen.dir/AutogenOldSettings.cmake"
+  "CommonBehavior.cpp"
+  "CommonBehavior.h"
+  "Laser.cpp"
+  "Laser.h"
+  "GenericBase.cpp"
+  "GenericBase.h"
+  "DifferentialRobot.cpp"
+  "DifferentialRobot.h"
+  "RCISMousePicker.cpp"
+  "RCISMousePicker.h"
+  "ui_mainUI.h"
+  "CMakeFiles/chocachoca.dir/specificworker.cpp.o"
+  "CMakeFiles/chocachoca.dir/specificmonitor.cpp.o"
+  "CMakeFiles/chocachoca.dir/home/jorge/robocomp/classes/rapplication/rapplication.cpp.o"
+  "CMakeFiles/chocachoca.dir/home/jorge/robocomp/classes/sigwatch/sigwatch.cpp.o"
+  "CMakeFiles/chocachoca.dir/home/jorge/robocomp/classes/qlog/qlog.cpp.o"
+  "CMakeFiles/chocachoca.dir/main.cpp.o"
+  "CMakeFiles/chocachoca.dir/genericmonitor.cpp.o"
+  "CMakeFiles/chocachoca.dir/commonbehaviorI.cpp.o"
+  "CMakeFiles/chocachoca.dir/genericworker.cpp.o"
+  "CMakeFiles/chocachoca.dir/rcismousepickerI.cpp.o"
+  "CMakeFiles/chocachoca.dir/CommonBehavior.cpp.o"
+  "CMakeFiles/chocachoca.dir/Laser.cpp.o"
+  "CMakeFiles/chocachoca.dir/GenericBase.cpp.o"
+  "CMakeFiles/chocachoca.dir/DifferentialRobot.cpp.o"
+  "CMakeFiles/chocachoca.dir/RCISMousePicker.cpp.o"
+  "CMakeFiles/chocachoca.dir/chocachoca_autogen/mocs_compilation.cpp.o"
+  "../bin/chocachoca.pdb"
+  "../bin/chocachoca"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chocachoca.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ practica5/src/specificworker.cpp
@@ -0,0 +1,306 @@
+/*
+ *    Copyright (C)2018 by YOUR NAME HERE
+ *
+ *    This file is part of RoboComp
+ *
+ *    RoboComp is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    RoboComp is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with RoboComp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "specificworker.h"
+
+/**
+* \brief Default constructor
+*/
+SpecificWorker::SpecificWorker(MapPrx& mprx) : GenericWorker(mprx)
+{
+	std::cout << std::boolalpha;   
+}
+
+/**
+* \brief Default destructor
+*/
+SpecificWorker::~SpecificWorker()
+{}
+
+bool SpecificWorker::setParams(RoboCompCommonBehavior::ParameterList params)
+{
+	try
+	{
+		RoboCompCommonBehavior::Parameter par = params.at("InnerModelPath");
+		innerModel = std::make_shared<InnerModel>(par.value);
+	}
+	catch(std::exception e) { qFatal("Error reading config params"); }
+
+	qDebug() << __FILE__ ;
+	
+	// Scene
+	scene.setSceneRect(-2500, -2500, 5000, 5000);
+	view.setScene(&scene);
+	view.scale(1, -1);
+	view.setParent(scrollArea);
+	//view.setViewport(new QGLWidget(QGLFormat(QGL::SampleBuffers)));
+	view.fitInView(scene.sceneRect(), Qt::KeepAspectRatio );
+
+	grid.initialize( TDim{ tilesize, -2500, 2500, -2500, 2500}, TCell{0, true, false, nullptr, 0.} );
+	
+	for(auto &[key, value] : grid)
+	{
+		auto tile = scene.addRect(-tilesize/2,-tilesize/2, 100,100, QPen(Qt::NoPen));
+		tile->setPos(key.x,key.z);
+		value.rect = tile;
+	}
+
+	robot = scene.addRect(QRectF(-200, -200, 400, 400), QPen(), QBrush(Qt::blue));
+	noserobot = new QGraphicsEllipseItem(-50,100, 100,100, robot);
+	noserobot->setBrush(Qt::magenta);
+
+	target = QVec::vec3(0,0,0);
+	
+	//qDebug() << __FILE__ << __FUNCTION__ << "CPP " << __cplusplus;
+	
+	connect(buttonSave, SIGNAL(clicked()), this, SLOT(saveToFile()));
+	connect(buttonRead, SIGNAL(clicked()), this, SLOT(readFromFile()));
+	
+	timer.start();
+	
+	return true;
+}
+
+void SpecificWorker::compute()
+{
+    
+	static RoboCompGenericBase::TBaseState bState;
+    
+ 	try
+ 	{
+        
+ 		differentialrobot_proxy->getBaseState(bState);
+		innerModel->updateTransformValues("base", bState.x, 0, bState.z, 0, bState.alpha, 0);
+		RoboCompLaser::TLaserData ldata = laser_proxy->getLaserData();
+		
+		//draw robot
+		robot->setPos(bState.x, bState.z);
+		robot->setRotation(-180.*bState.alpha/M_PI);
+		
+		//updateVisitedCells(bState.x, bState.z);
+		updateOccupiedCells(bState, ldata);
+        //
+        //
+        //
+		
+		if(targetReady)
+		{
+			if(planReady)
+			{
+				if(path.empty())
+				{
+					qDebug() << "Arrived to target";
+					differentialrobot_proxy->stopBase();
+					targetReady = false; 
+				}
+				else
+						if(innerModel->transform("base", QVec::vec3(currentPoint.x(), 0, currentPoint.z()),"world").norm2() < 150)
+						{
+							currentPoint = path.front();
+							path.pop_front();
+							state = State::ALINEAR;
+						}
+						else
+						{
+							
+							QVec r = innerModel->transform("base", QVec::vec3(currentPoint.x(), 0, currentPoint.z()),"world");
+							float distancia = r.norm2();
+							float angulo = atan2(r.z(), r.x());
+							std::cout<<distancia<<std::endl;
+							float rotMax = 0.5;
+							float k = 1;
+							
+							switch(state){
+								
+								case State::ALINEAR:{
+									if (angulo < 1.6 && angulo > 1.5) //Si ya está alineado
+									{
+										state = State::GOTO;
+										return;
+									}
+									
+									if(angulo > 0.5 && angulo < 2.5)
+                                        k = pow((angulo - 1.5), 2) + 0.5;
+                                    else
+                                        k = 1;
+									
+									if (abs(angulo) > 1.5)
+										k = -1;
+									
+									differentialrobot_proxy->setSpeedBase(0, k*rotMax);
+								}break;
+									
+								case State::GOTO:
+                                    float vAvance  = 400;
+                                    float vRot;
+									QVec rel = innerModel->transform("base", QVec::vec3(target.x(), 0, target.z()),"world");
+                                    float dist = rel.norm2();
+									 if( dist > 50){
+                                            vRot = atan2(rel.x(), rel.z());
+    
+                                     if(vRot > 0.5){
+                                            vRot = 0.5;
+                                     }
+    
+                                     if(vRot< -0.5){
+                                            vRot= -0.5;
+                                     }
+                                     differentialrobot_proxy->setSpeedBase(vAvance,vRot);
+        
+                                     }else{
+                                     state = State::ALINEAR;
+                                     differentialrobot_proxy->setSpeedBase(0,0);
+                                     targetReady = false;
+                                     return;
+                                     }break;
+                                     
+							}
+						}
+			}
+
+			else
+			{
+				qDebug() << bState.x << bState.z << target.x() << target.z() ;
+				path = grid.getOptimalPath(QVec::vec3(bState.x,0,bState.z), target);
+				for(auto &p: path)
+					greenPath.push_back(scene.addEllipse(p.x(),p.z(), 100, 100, QPen(Qt::green), QBrush(Qt::green)));
+				planReady = true;
+			}
+		}
+	}
+ 	catch(const Ice::Exception &e)
+	{	std::cout  << e << std::endl; }
+	
+	//Resize world widget if necessary, and render the world
+	if (view.size() != scrollArea->size())
+			view.setFixedSize(scrollArea->width(), scrollArea->height());
+	draw();
+	
+}
+
+void SpecificWorker::saveToFile()
+{
+	grid.saveToFile(fileName);
+}
+
+void SpecificWorker::readFromFile()
+{
+	std::ifstream myfile;
+	myfile.open(fileName, std::ifstream::in);
+	
+	if(!myfile.fail())
+	{
+		//grid.initialize( TDim{ tilesize, -2500, 2500, -2500, 2500}, TCell{true, false, nullptr} );
+		for( auto &[k,v] : grid)
+			delete v.rect;
+		grid.clear();
+		Grid<TCell>::Key key; TCell value;
+		myfile >> key >> value;
+		int k=0;
+		while(!myfile.eof()) 
+		{
+			auto tile = scene.addRect(-tilesize/2,-tilesize/2, 100,100, QPen(Qt::NoPen));;
+			tile->setPos(key.x,key.z);
+			value.rect = tile;
+			value.id = k++;
+			value.cost = 1;
+			grid.insert<TCell>(key,value);
+			myfile >> key >> value;
+		}
+		myfile.close();	
+		robot->setZValue(1);
+		std::cout << grid.size() << " elements read to grid " << fileName << std::endl;
+	}
+	else
+		throw std::runtime_error("Cannot open file");
+}
+
+void SpecificWorker::checkTransform(const RoboCompGenericBase::TBaseState &bState)
+{
+	auto r = innerModel->transform("base", target, "world");		// using InnerModel
+	
+	Rot2D rot(bState.alpha);																		// create a 2D clockwise rotation matrix
+	QVec t = QVec::vec2(bState.x, bState.z);									  // create a 2D vector for robot translation
+	QVec t2 = QVec::vec2(target.x(), target.z());								// create a 2D vector from the 3D target
+	QVec q = rot.transpose() * ( t2 - t);												// multiply R_t * (y - T)
+	qDebug() << target << r << q;
+}
+
+void SpecificWorker::updateOccupiedCells(const RoboCompGenericBase::TBaseState &bState, const RoboCompLaser::TLaserData &ldata)
+{
+	auto *n = innerModel->getNode<InnerModelLaser>("laser");
+	for(auto l: ldata)
+	{
+		auto r = n->laserTo("world", l.dist, l.angle);	// r is in world reference system
+		// we set the cell corresponding to r as occupied 
+		auto [valid, cell] = grid.getCell(r.x(), r.z()); 
+		if(valid)
+			cell.free = false;
+	}
+}
+
+void SpecificWorker::updateVisitedCells(int x, int z)
+{
+	static unsigned int cont = 0;
+	auto [valid, cell] = grid.getCell(x, z); 
+	if(valid)
+	{
+		auto &occupied = cell.visited;
+		if(occupied)
+		{
+			occupied = false;
+			cont++;
+		}
+		float percentOccupacy = 100. * cont / grid.size();
+	}
+}
+
+void SpecificWorker::draw()
+{
+	for(auto &[key, value] : grid)
+	{
+// 		if(value.visited == false)
+// 			value.rect->setBrush(Qt::lightGray);
+		if(value.free == false)
+			value.rect->setBrush(Qt::darkRed);
+	}
+	view.show();
+}
+
+/////////////// PATH PLANNING /////7
+
+
+
+
+/////////////////////////////////////////////////////////77
+/////////
+//////////////////////////////////////////////////////////
+
+void SpecificWorker::setPick(const Pick &myPick)
+{
+	target[0] = myPick.x;
+	target[2] = myPick.z;
+	target[1] = 0;
+	qDebug() << __FILE__ << __FUNCTION__ << myPick.x << myPick.z ;
+	targetReady = true;
+	planReady = false;
+	for(auto gp: greenPath)
+		delete gp;
+	greenPath.clear();
+	
+}
